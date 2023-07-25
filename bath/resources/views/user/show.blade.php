7d02146a98e3cdcd33be7f91061ce72b3c768a6e
石川倖太 <ishikawakouta@korokoroMacBook-Air.local>
2023-07-25 17:59:28 +0900

    差分チェック

--- bath/resources/views/user/show.blade.php
+++ bath/resources/views/user/show.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            (diff)銭湯の詳細
+            銭湯の詳細
+            (diff)
         </h2>
     </x-slot>
 
