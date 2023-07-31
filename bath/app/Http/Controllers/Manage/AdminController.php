6147e1dd3da4f3f97b0cc4f7eb1d7dc8d3de182d
石川倖太 <ishikawakouta@korokoroMacBook-Air.local>
2023-07-31 11:58:33 +0900

    manageの管理者create

--- bath/app/Http/Controllers/Manage/AdminController.php
+++ bath/app/Http/Controllers/Manage/AdminController.php
@@ -33,7 +33,7 @@ class AdminController extends Controller
      */
     public function create()
     {
-        //
+        return view('manage.admin.create');
     }
 
     /**
--- bath/resources/views/manage/admin/index.blade.php
+++ bath/resources/views/manage/admin/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('管理者一覧') }}
         </h2>
     </x-slot>
 
@@ -9,13 +9,37 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    管理者一覧
-                    @foreach ($admins as $admin)
-                        {{ $admin->name }}
-                        {{ $admin->email }}
-                        {{ $admin->created_at->diffForHumans() }}
-                    <br>
-                    @endforeach
+                    <section class="text-gray-600 body-font">
+                        <div class="container px-5 mx-auto">
+                            <div class="flex justify-end mb-4">
+                              <button onclick="location.href='{{route('manage.admin.create')}}'" class="text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">新規登録</button>
+                            </div>
+                          <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+                            <table class="table-auto w-full text-left whitespace-no-wrap">
+                              <thead>
+                                <tr>
+                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
+                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メールアドレス</th>
+                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">作成日</th>
+                                  <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                @foreach ($admins as $admin)
+                                <tr>
+                                  <td class="px-4 py-3">{{ $admin->name }}</td>
+                                  <td class="px-4 py-3">{{ $admin->email }}</td>
+                                  <td class="px-4 py-3">{{ $admin->created_at->diffForHumans() }}</td>
+                                  <td class="w-10 text-center">
+                                    <input name="plan" type="radio">
+                                  </td>
+                                </tr>
+                                @endforeach
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
+                      </section>
                 </div>
             </div>
         </div>
