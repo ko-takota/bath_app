98ff7c027aa38d0fa84a3d4675928a9e222f37d1
石川倖太 <ishikawakouta@korokoroMacBook-Air.local>
2023-08-18 17:30:37 +0900

    カートから施設一覧

--- bath/resources/views/user/cart/mycart.blade.php
+++ bath/resources/views/user/cart/mycart.blade.php
@@ -27,19 +27,21 @@
                                     @if (count($carts) > 0)
                                         @foreach ($carts->sortByDesc('created_at') as $cart)
                                             @if($cart->plan) <!-- プランが存在する場合 -->
-                                                <div class="flex-shrink-0 w-72 bg-yellow-200 shadow-md p-8 rounded-lg m-24">
-                                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{ $cart->plan->bath->name }}<br>プラン名：{{ $cart->plan->name }}</h1>
-                                                    <p class="font-medium text-gray-900 mt-3">{{ $cart->plan->contents }}</p>
-                                                    <p class="leading-relaxed mb-5">{{ $cart->plan->price }}<span class="text-gray-500">(月額)</span></p>
-                                                    <form method="post" action="{{route('user.cart.delete', ['item' => $cart->id])}}" class="mt-12">
-                                                        @csrf
-                                                        <button>
-                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                                            </svg>{{-- ゴミ箱アイコン --}}
-                                                        </button>
-                                                    </form>
-                                                </div>
+                                                <a href="{{ route('user.item.show', ['item' => $cart->bath_id ])}}">
+                                                    <div class="flex-shrink-0 w-72 bg-yellow-200 shadow-md p-8 rounded-lg m-24">
+                                                        <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{ $cart->plan->bath->name }}<br>プラン名：{{ $cart->plan->name }}</h1>
+                                                        <p class="font-medium text-gray-900 mt-3">{{ $cart->plan->contents }}</p>
+                                                        <p class="leading-relaxed mb-5">{{ $cart->plan->price }}<span class="text-gray-500">(月額)</span></p>
+                                                        <form method="post" action="{{route('user.cart.delete', ['item' => $cart->id])}}" class="mt-12">
+                                                            @csrf
+                                                            <button>
+                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                                                </svg>{{-- ゴミ箱アイコン --}}
+                                                            </button>
+                                                        </form>
+                                                    </div>
+                                                </a>
                                             @else
                                             <!-- プランが論理削除された場合の表示 -->
                                             <p>プランは削除されたため表示されません。</p>
