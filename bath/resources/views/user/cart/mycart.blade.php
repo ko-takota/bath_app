c3ebc2a55082afc21a0d9a7a01b1a55387535fb2
石川倖太 <ishikawakouta@korokoroMacBook-Air.local>
2023-08-19 11:03:48 +0900

    購入ボタン

--- bath/resources/views/user/cart/mycart.blade.php
+++ bath/resources/views/user/cart/mycart.blade.php
@@ -35,7 +35,7 @@
                                                         <form method="post" action="{{route('user.cart.delete', ['item' => $cart->id])}}" class="mt-12">
                                                             @csrf
                                                             <button>
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:scale-150">
                                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                                 </svg>{{-- ゴミ箱アイコン --}}
                                                             </button>
@@ -55,12 +55,15 @@
                         </div>
                     </div>
                 </div>
+
                 <div class="my-2 bg-white">
                     合計：{{ number_format($totalPrice) }}<span class="text-sm">円(月額)</span>
                 </div>
+                @if (count($carts) > 0)
                 <div>
                     <button onclick="location.href='{{ route('user.cart.pay') }}'" class="bg-yellow-400 border-0 py-2 px-3 focus:outline-none hover:bg-yellow-600 rounded">購入する</button>
                 </div>
+                @endif
             </div>
             <button class="bg-gray-400 active:bg-gray-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <a href="{{route('user.search')}}" class="index bg-gray-400 hover:text-gray-300">施設一覧へ</a>
