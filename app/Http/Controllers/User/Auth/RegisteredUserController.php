d38d6c32e94ade339487846cb2ef7d2d1dfef16e
EC2 Default User <ec2-user@ip-172-31-31-59.us-east-2.compute.internal>
2023-09-04 07:35:24 +0000

    デフォルトパスワードルールの定義戻し

--- app/Http/Controllers/User/Auth/RegisteredUserController.php
+++ app/Http/Controllers/User/Auth/RegisteredUserController.php
@@ -33,7 +33,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', 'regex:/^[A-Za-z0-9]+$/u', Password::defaults()],
+            'password' => ['required', 'confirmed', 'regex:/^[A-Za-z0-9]+$/u', Rules\Password::min(8)->mixedCase()->numbers()],
         ]);
 
         $user = User::create([
